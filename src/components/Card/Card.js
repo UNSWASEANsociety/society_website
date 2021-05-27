f0c280ecd3c3e7bad596cf05598305d217418a9b
Arthur <ArthurWang2000@hotmail.com>
2021-05-27 14:15:00 +1000

    Made card component enlarge when hovered over

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -26,11 +26,6 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
   cardMedia: {
     paddingTop: "56.25%", // 16:9
   },
@@ -46,7 +41,7 @@ const useStyles = makeStyles((theme) => ({
 const Card = ({ image, to, children }) => {
   const classes = useStyles();
   const card = (
-    <MUICard className={classes.card}>
+    <MUICard className={styles.card}>
       <CardMedia
         className={classes.cardMedia}
         image={image}
