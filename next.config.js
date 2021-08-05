9be908ffed33910bf024270d806f938b502d09a0
Arthur Wang <ArthurWang2000@Hotmail.com>
2021-08-06 00:50:45 +1000

    Attempt to host to netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com', 'images.squarespace-cdn.com',],
+    domains: [
+      "images.unsplash.com",
+      "source.unsplash.com",
+      "images.squarespace-cdn.com",
+    ],
   },
-}
+  target: "serverless",
+};
