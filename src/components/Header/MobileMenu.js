83861e63c6120568d017f908b73aa8bb5fef4a50
Antonius <Antonius@student.unsw.edu.au>
2025-06-20 23:05:44 +1000

    added-semi-transparent-header

--- src/components/Header/MobileMenu.js
+++ src/components/Header/MobileMenu.js
@@ -12,14 +12,14 @@ import MenuStyle from "./MobileMenu.module.scss";
 const MobileMenu = ({links}) => {
   const styles = {
     bmBurgerButton: {
-      position: "fixed", // have to make this fixed for some reason
+      position: "fixed",
       width: "36px",
       height: "30px",
-      left: getWidth() - 50 + "px",
+      right: "1.5rem",
       top: "1rem",
     },
     bmBurgerBars: {
-      background: "#373a47",
+      background: "#fff",
     },
     bmCrossButton: {
       height: "24px",
@@ -29,19 +29,39 @@ const MobileMenu = ({links}) => {
       background: "#bdc3c7",
     },
     bmMenu: {
-      background: "#373a47",
+      background: "rgba(34, 40, 49, 0.85)",
+      backdropFilter: "blur(12px) saturate(160%)",
       padding: "2.5em 1.5em 0",
       fontSize: "1.15em",
+      boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.18)",
+      position: "fixed",
+      top: 0,
+      bottom: 0,
+      height: "100vh",
+      width: "100vw",
+      left: 0,
+      right: 0,
+      borderRadius: 0,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
     },
     bmMorphShape: {
       fill: "#373a47",
     },
     bmItemList: {
-      color: "#b8b7ad",
-      padding: "0.8em",
+      color: "#fff",
+      padding: "0.8em 2.5em",
+      width: "100%",
+      textAlign: "left",
     },
     bmOverlay: {
       background: "rgba(0, 0, 0, 0.3)",
+      top: 0,
+      left: 0,
+      height: "100vh",
+      width: "100vw",
     },
   };
 
