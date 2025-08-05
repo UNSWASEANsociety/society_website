10541a5401aee48c5abcf73751c513f265dd6532
Antonius <Antonius@student.unsw.edu.au>
2025-08-05 22:18:39 +1000

    added past and upcoming navbar

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,13 +1,26 @@
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { Block, ParallaxBlock } from "../../components/Block";
 import { Card } from "../../components/Card";
+import { EventsNavBar } from "../../components/EventsNavBar";
+import { filterEvents, sortEventsByDate } from "../../utils/eventFilters";
 import getEvents from "./events_data";
 
 
 export default function Events() {
-  const events = getEvents();
+  const [activeFilter, setActiveFilter] = useState("all");
+  const allEvents = getEvents();
+
+  const filteredEvents = useMemo(() => {
+    const filtered = filterEvents(allEvents, activeFilter);
+    return sortEventsByDate(filtered, activeFilter === "upcoming");
+  }, [allEvents, activeFilter]);
+
+  const handleFilterChange = (filter) => {
+    setActiveFilter(filter);
+  };
+
   return (
     <>
       <main>
@@ -20,9 +33,15 @@ export default function Events() {
             <HTimeLine />
           </Hidden> */}
         </ParallaxBlock>
+
+        <EventsNavBar
+          activeFilter={activeFilter}
+          onFilterChange={handleFilterChange}
+        />
+
         <Block>
           <Grid container spacing={4}>
-            {events.map((event) => (
+            {filteredEvents.map((event) => (
               <Card to={`/event/${event.id}`} image={event.image} key={event.id}>
                 <Typography gutterBottom variant="h5" component="h2">
                   {event.name}
@@ -33,6 +52,20 @@ export default function Events() {
               </Card>
             ))}
           </Grid>
+
+          {filteredEvents.length === 0 && (
+            <Typography
+              variant="h6"
+              align="center"
+              style={{
+                opacity: 0.7,
+                marginTop: "2rem",
+                fontStyle: "italic"
+              }}
+            >
+              No {activeFilter === "all" ? "" : activeFilter} events found.
+            </Typography>
+          )}
         </Block>
       </main>
     </>
